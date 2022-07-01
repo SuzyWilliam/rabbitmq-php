f380a35d41c06762c701b2ddd42a974aa0f4f039
suzy.w <suzy.william@linkdev.com>
2022-07-02 00:56:08 +0200

    Created Chat app

---
+++ ChatApp/index.php
@@ -0,0 +1,38 @@
+#!/usr/bin/php
+<?php
+stream_set_blocking(STDIN, false);
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Exchange\AMQPExchangeType;
+use PhpAmqpLib\Message\AMQPMessage;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$connection = new AMQPStreamConnection('localhost', 5672, "guest", "guest");
+$channel = $connection->channel();
+
+$channel->exchange_declare('chat_exchange', AMQPExchangeType::FANOUT, false, true, true);
+
+list($queue_name,,) = $channel->queue_declare('', false, false, true, false);
+$channel->queue_bind($queue_name, 'chat_exchange', '');
+
+echo " [*] Waiting for logs. To exit press CTRL+C\n";
+
+$channel->basic_consume($queue_name, '', false, true, false, false, function (AMQPMessage $msg) {
+    echo ' [x] Recieved: ', $msg->body;
+});
+
+
+$data = fread(STDIN,1028);
+
+while ($data !== "" || $channel->is_consuming()) {
+    if ($data !== "") {
+        $msg = new AMQPMessage($data);
+        
+        $channel->basic_publish($msg, 'chat_exchange');
+        // echo ' [x] Sent: ', $data, "\n";
+    }   
+    $data = fread(STDIN,1028);
+    $channel->wait(null,true);
+}
+$channel->close();
+$connection->close();
--- README.md
+++ README.md
@@ -11,3 +11,36 @@ RabbitMQ is one of the most popular open-source message brokers in use today. Th
   * Setting up and using authentication
   * Authorizing and blocking application actions
   * Tracking message contents for troubleshooting
+---
+
+## Steps to:
+- Open a connection:
+
+    `$connection = new AMQPStreamConnection('localhost',5672,"guest","guest");`
+- Open a channel:
+  
+  `$channel = $connection->channel();`
+- Declare an exchange:
+  
+  `$channel->exchange_declare('chat_exchange', AMQPExchangeType::FANOUT,false,true,true);`
+- Write message:
+  
+  `$msg = new AMQPMessage($data);`
+- Publish message:
+  
+  `$channel->basic_publish($msg, 'chat_exchange');`
+- Declare a queue:
+
+  `$queue_name = $channel->queue_declare('chat_queue',false,true,false);`
+
+- Bind a queue to an exchange:
+
+  `$channel->queue_bind('chat_queue', 'chat_exchange');`
+
+- Consume messages:
+
+  `$channel->basic_consume('chat_queue','',false,true,false,false,function(AMQPMessage $msg){ echo ' [x] ', $msg->body, "\n";});`
+
+- Keep reading messages:
+
+  ``
